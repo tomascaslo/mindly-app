84f9ae7cc790d3447fb3d1505236792de24f9674
Tomas Castro <tomas@home-pc.lan>
2014-05-31 21:26:38 -0500

    Reorganize folder structure; Started mindly app main configuration in app.js; Edited gulpfile to compile jades and runserver

--- mindly-app/gulpfile.js
+++ mindly-app/gulpfile.js
@@ -1,12 +1,30 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var sh = require('shelljs');
+var jade = require('gulp-jade');
+var connect = require('gulp-connect');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+	sass: ['./scss/**/*.scss'],
+	jade: './jade/**/*.jade',
+	views: './www/',
 };
+
+gulp.task('default', ['sass', 'jade']);
+
+gulp.task('jade', function() {
+	gulp.src(paths.jade)
+		.pipe(jade({
+			pretty: true,
+		}))
+		.pipe(gulp.dest(paths.views))
+		.pipe(connect.reload());
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -21,7 +39,35 @@ gulp.task('sass', function(done) {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+	gulp.watch(paths.sass, ['sass']);
+	gulp.watch(paths.jade, ['jade']);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'www',
+		livereload: true,
+	});
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
+
+gulp.task('server', ['connect', 'watch']);
--- mindly-app/www/js/app.js
+++ mindly-app/www/js/app.js
@@ -1,5 +1,4 @@
 'use-strict';
-
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
@@ -7,24 +6,50 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('mindlyApp', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('mindly', [
+    'ionic',
+    ])
 
-.run(function($ionicPlatform) {
+.run(['$rootScope', '$ionicPlatform', function($rootScope, $ionicPlatform) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
-})
+}])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(['$httpProvider', '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $httpProvider.defaults.headers.patch = {
+        'Content-Type': 'application/json;charset=utf-8',
+    };
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
+  
+ 	.state('menu', {
+ 		url: "/menu",
+ 		abstract: true,
+ 		templateUrl: "views/menu.html",
+ 		controller: 'MenuCtrl',
+ 	})
+
+ 	.state('home', {
+ 		url: "/menu/home",
+ 		templateUrl: "views/home.html",
+ 		controller: 'HomeCtrl', 
+ 	})
 
     // setup an abstract state for the tabs directive
     .state('tab', {
@@ -75,7 +100,7 @@ angular.module('mindlyApp', ['ionic', 'starter.controllers', 'starter.services']
     })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/menu/home');
 
-});
+}]);
 

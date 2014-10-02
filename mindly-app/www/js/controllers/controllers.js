7c3235a6f94d8608a19310f5273bfae78ea45782
Tomas Castro <tomas.caslo90@gmail.com>
2014-10-01 21:59:53 -0500

    Upload to github

---
+++ mindly-app/www/js/controllers/controllers.js
@@ -0,0 +1,17 @@
+'use-strict';
+
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
